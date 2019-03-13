b218af7df0e897e050f19fe235b961f7746390ce
ahmaddregat <33619392+ahmaddregat@users.noreply.github.com>
2019-03-13 02:42:38 +0200

    Add files via upload

---
+++ Ex1/case0/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * Reads two numbers from cin and writes their product to cout.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout << "all good" << endl;
+}
